7c6c1fc1f119572b87ed32b15ba6a52f3ccb0f0f
Juanba2002 <bajuan00@gmail.com>
2023-08-31 10:09:13 -0500

    Correccion imagen

--- app/src/main/java/com/example/tuhobbie/AgregarCancha.kt
+++ app/src/main/java/com/example/tuhobbie/AgregarCancha.kt
@@ -1,11 +1,48 @@
 package com.example.tuhobbie
 
+import android.content.pm.PackageManager
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class AgregarCancha : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agregar_cancha)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+
+            }
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMARA)
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        val text: TextView=findViewById(R.id.textView4)
+        if (requestCode == CAMARA) {
+            for (i in permissions.indices) {
+                val permission = permissions[i]
+                val grantResult = grantResults[i]
+                if (permission == Manifest.permission.CAMERA) {
+                    if (grantResult == PackageManager.PERMISSION_GRANTED) {
+                        text.text = "PERMISO DE CÁMARA DADO"
+                    } else {
+                        text.text = "PERMISO DE CÁMARA NO DADO"
+                    }
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val CONTACTOS = 0
+        const val CAMARA = 1
     }
 }
--- app/src/main/java/com/example/tuhobbie/ListaAmigos.kt
+++ app/src/main/java/com/example/tuhobbie/ListaAmigos.kt
@@ -1,11 +1,51 @@
 package com.example.tuhobbie
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class ListaAmigos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_amigos)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)!=PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_CONTACTS)){
+
+            }
+        }
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_CONTACTS), CONTACTOS)
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        val text:TextView=findViewById(R.id.Text)
+        if(requestCode== CONTACTOS){
+            for (i in permissions.indices){
+                val permission=permissions[i]
+                val grantResult=grantResults[i]
+                if(permission==Manifest.permission.READ_CONTACTS){
+                    if(grantResult==PackageManager.PERMISSION_GRANTED){
+                      text.text="PERMISO DADO"
+                    }else{
+                        text.text="PERMISO NO DADO"
+                    }
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val CONTACTOS=0
+        private const val CAMARA=1
+    }
+
 }
